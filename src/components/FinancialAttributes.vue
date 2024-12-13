94ec4fc3a2b46d4f31a02ad16556556379ffee36
mogs.git <mogs.mail1@gmail.com>
2024-12-13 17:43:38 +0000

    fin attributes

---
+++ src/components/FinancialAttributes.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { usePointsStore } from '@/stores/points';
+let pointsStore = usePointsStore()
+</script>
+
+<template>
+     <div v-for="(point, index) in pointsStore.points" :key="'input-' + index" style="margin-top: 10px;">
+      <label>
+        Point {{ index + 1 }}:
+        X = {{ point.x }},
+        Recurring = {{ point.recurring }}
+        Value: 
+        <input
+          type="number"
+          v-model.number="point.value"
+          :min="0"
+          style="width: 60px;"
+        />
+
+        <input
+          type="boolean"
+          v-model.boolean="point.recurring"
+          style="width: 60px;"
+        />
+
+        <select id="store-type" v-model="point.type">
+      <option value="income">Income</option>
+      <option value="expense">Expense</option>
+    </select>
+      </label>
+    </div>
+</template>
