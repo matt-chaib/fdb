05a8ee80994b32eb270f3f0253dfbc617c9cd15e
mogs.git <mogs.mail1@gmail.com>
2024-12-13 17:45:54 +0000

    view

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -3,13 +3,32 @@ import Slider from '@/components/Slider.vue';
 import Chart from '../components/Chart.vue'
 
 import { usePointsStore } from '@/stores/points'; // Import the store
+import FinanceAttributes from '@/components/FinanceAttributes.vue';
+import FinanceSummary from '@/components/FinanceSummary.vue';
 const pointStore = usePointsStore(); // Get the store instance
 
 </script>
 
 <template>
   <main>
-    <Slider :pointsStore="pointStore" />
-    <Chart :pointStore="pointStore"/>
+    <div class="attributes">
+      <FinanceAttributes />
+      <FinanceSummary />
+    </div>
+    <div>
+      <Slider :pointsStore="pointStore" />
+      <Chart :pointStore="pointStore"/>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: row;
+}
+
+.attributes {
+  min-width: 300px;
+}
+</style>
